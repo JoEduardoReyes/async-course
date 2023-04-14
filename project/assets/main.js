acb327936c52702f656a666537eaba5f22a50c64
Eduardo Reyes <joeduardoreyes@outlook.com>
2023-04-14 09:59:55 -0600

    Implementamos la API en el documento correctamente

--- project/assets/main.js
+++ project/assets/main.js
@@ -20,30 +20,28 @@ async function fetchData(urlAPI) {
 (async () => {
 	try {
 		const videos = await fetchData(API);
-		// let view = `
-		// ${videos.contents
-		// 	.map(
-		// 		(video) => `
-		// <div class="group relative">
-		// 	<div
-		// 		class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none"
-		// 	>
-		//   <img src="${video.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full" />
-		// 	</div>
-		// 	<div class="mt-4 flex justify-between">
-		// 	  <h3 class="text-sm text-gray-700">
-		// 	    <span aria-hidden="true" class="absolute inset-0"></span>
-		// 	    ${video.snippet.title}
-		// 	  </h3>
-		//   </div>
-		// </div>
-		// `
-		// 	)
-		// 	.slice(0, 4)
-		// 	.join("")}
-
-		// `;
-		// content.innerHTML = view;
+		// console.log(videos.contents[0].video.thumbnails[3].url);
+		let view = `
+    ${videos.contents
+			.map(
+				(item) => `
+      <div class="group relative">
+        <div
+          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+          <img src="${item.video.thumbnails[3].url}" alt="${item.video.title}" class="w-full">
+        </div>
+        <div class="mt-4 flex justify-between">
+          <h3 class="text-sm text-gray-700">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+            ${item.video.title}
+          </h3>
+        </div>
+      </div>
+    `
+			)
+			.slice(0, 4)
+			.join("")}`;
+		content.innerHTML = view;
 	} catch (error) {
 		console.error(error);
 	}
